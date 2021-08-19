2d360e0d283f5aa4744ac7f68e9a711a76b4f58b
sgt-dope <sgtdopeaf@gmail.com>
2021-08-19 09:23:35 +0000

    added user and client creation

--- keycloak_admin.py
+++ keycloak_admin.py
@@ -7,19 +7,29 @@ cmnd = os.popen("gp url 8080")
 gitpod_url = cmnd.read().strip() + "/auth/"
 
 
-keycloak_admin = KeycloakAdmin(server_url="https://8080-gold-fish-93s37204.ws-eu15.gitpod.io/auth/",
+keycloak_admin = KeycloakAdmin(server_url=gitpod_url,
                                username='admin',
                                password='admin',
                                realm_name="master",
                                verify=True)
 
 
+
 realm_name = "flask-demo"
-keycloak_admin.create_realm(payload={"realm": "flask-demo"}, skip_exists=False)
+keycloak_admin.create_realm(payload={"realm": "flask-demo",
+                                    "enabled" : True}, skip_exists=False)
 
 keycloak_admin.realm_name = realm_name
 
-keycloak_admin.create_client(payload={"clientId": "hello_app"}, skip_exists=False)
+client_name = "flask-app"
+keycloak_admin.create_client(payload={"clientId": client_name,
+                                        "enabled" : True,
+                                        "directAccessGrantsEnabled" : True,
+                                        "redirectUris":["*"],
+                                        "protocol": "openid-connect"}, skip_exists=False)
+
+# Setting up the protocol to be openid-connect will automatically make the Access Type Public
+# It'll also set the Backchannel Logout Session Required to ON
 
 new_user = keycloak_admin.create_user({"email": "kenannakola@hotmail.com",
                     "username": "kenannakola@hotmail.com",
